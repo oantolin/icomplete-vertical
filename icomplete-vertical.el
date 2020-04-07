5c55518944b5864adee73f7ac1c2f4cd6305a297
Omar Antolín <omar.antolin@gmail.com>
2020-04-07 16:25:58 -0500

    Document icomplete-vertical-separator variable and face

--- icomplete-vertical.el
+++ icomplete-vertical.el
@@ -32,6 +32,23 @@
 ;; 2. The first candidate appears on the same line as the one you are
 ;;    typing in.  This makes it harder to visually scan the candidates
 ;;    as the first one starts in a different column from the others.
+;;
+;; For users that prefer the traditional horizontal mode for Icomplete
+;; but want to define some commands that use vertical completion, this
+;; package provides the `icomplete-vertical-do' macro.  For example to
+;; define a command to yank from the kill-ring using completion:
+;;
+;; (defun insert-kill-ring-item ()
+;;   "Insert item from kill-ring, selected with completion."
+;;   (interactive)
+;;   (icomplete-vertical-do (:separator "\n··········\n" :height 20)
+;;     (insert (completing-read "Yank: " kill-ring nil t))))
+;;
+;; Both the :separator and :height are optional and default to
+;; icomplete-vertical-separator and to
+;; icomplete-vertical-prospects-height, respectively.
+;; If you omit both parts you still need to include the empty
+;; parenthesis: (icomplete-vertical-do () ...)!.
 
 ;;; Code:
 
