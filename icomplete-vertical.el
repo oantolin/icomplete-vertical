1c3ac9bc74f72f39e1cc77b0c9b9b1f527a02809
Daniel Mendler <mail@daniel-mendler.de>
2021-02-26 07:39:50 +0100

    Bump Emacs version dependency

--- icomplete-vertical.el
+++ icomplete-vertical.el
@@ -6,7 +6,7 @@
 ;; Keywords: convenience, completion
 ;; Version: 0.3
 ;; Homepage: https://github.com/oantolin/icomplete-vertical
-;; Package-Requires: ((emacs "25.1"))
+;; Package-Requires: ((emacs "26.1"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
